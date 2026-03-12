3821c2fda575ca25b198e33ef31611efb6940f14
Роман Мстиславский <mstislavskijr@gmail.com>
2026-03-12 21:57:28 +0300

    fix all models

--- backend/network/models.py
+++ backend/network/models.py
@@ -2,17 +2,11 @@ from django.db import models
 
 
 class NetworkNode(models.Model):
-
     name = models.CharField(max_length=255)
-
     email = models.EmailField()
-
     country = models.CharField(max_length=255)
-
     city = models.CharField(max_length=255)
-
     street = models.CharField(max_length=255)
-
     house_number = models.CharField(max_length=20)
 
     supplier = models.ForeignKey(
@@ -20,26 +14,31 @@ class NetworkNode(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name="clients"
+        related_name="clients",
+        verbose_name="Поставщик",
     )
 
     debt = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=0
+        default=0,
+        verbose_name="Задолженность перед поставщиком",
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def level(self):
-        level = 0
-        supplier = self.supplier
-
-        while supplier:
-            level += 1
-            supplier = supplier.supplier
-
-        return level
+    class Meta:
+        verbose_name = "Звено сети"
+        verbose_name_plural = "Звенья сети"
 
     def __str__(self):
         return self.name
+
+    @property
+    def level(self):
+        level = 0
+        supplier = self.supplier
+        while supplier:
+            level += 1
+            supplier = supplier.supplier
+        return level
--- backend/products/models.py
+++ backend/products/models.py
@@ -3,18 +3,18 @@ from network.models import NetworkNode
 
 
 class Product(models.Model):
-
     name = models.CharField(max_length=255)
-
     model = models.CharField(max_length=255)
-
     release_date = models.DateField()
-
     network = models.ForeignKey(
         NetworkNode,
         related_name="products",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.model})"

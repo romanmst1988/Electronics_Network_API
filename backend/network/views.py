88909a7d0bf7188efc728f88d72eaf3203f04eeb
Роман Мстиславский <mstislavskijr@gmail.com>
2026-03-12 22:02:56 +0300

    add ViewSet с фильтрацией по стране

--- backend/network/views.py
+++ backend/network/views.py
@@ -1,20 +1,14 @@
-from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
 
 from .models import NetworkNode
 from .serializers import NetworkNodeSerializer
-
 from users.permissions import IsActiveEmployee
 
 
 class NetworkNodeViewSet(ModelViewSet):
-
-    queryset = NetworkNode.objects.all()
-
+    queryset = NetworkNode.objects.all().select_related("supplier")
     serializer_class = NetworkNodeSerializer
-
     permission_classes = [IsActiveEmployee]
-
     filter_backends = [DjangoFilterBackend]
-
-    filterset_fields = ["country"]
+    filterset_fields = ("country",)

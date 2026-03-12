3e9af8c4fdde9f5b0ccf0311557bba973dcc45be
Роман Мстиславский <mstislavskijr@gmail.com>
2026-03-12 21:58:51 +0300

    fix code serializers.network

--- backend/network/serializers.py
+++ backend/network/serializers.py
@@ -3,8 +3,21 @@ from .models import NetworkNode
 
 
 class NetworkNodeSerializer(serializers.ModelSerializer):
+    level = serializers.ReadOnlyField()
 
     class Meta:
         model = NetworkNode
-        fields = "__all__"
-        read_only_fields = ("debt", "created_at")
+        fields = (
+            "id",
+            "name",
+            "email",
+            "country",
+            "city",
+            "street",
+            "house_number",
+            "supplier",
+            "debt",
+            "created_at",
+            "level",
+        )
+        read_only_fields = ("created_at", "level")

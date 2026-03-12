14f433112055cb6b42aa2ca9a8ce22ac7d11a92d
Роман Мстиславский <mstislavskijr@gmail.com>
2026-03-12 21:38:27 +0300

    fix app products.admin.py

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "model", "release_date")
+    list_display = ("id", "name", "model", "release_date", "network")
+    list_filter = ("release_date",)
+    search_fields = ("name", "model")
